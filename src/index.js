2af27ec6352e82acc75a990a28e850c66baba50e
Nathan <playcracktheskyy@gmail.com>
2018-02-16 02:22:41 +0000

    updated resource pager

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import SmartDevQuery from './containers/SmartDevQuery'
 import DataViewer from './components/DataViewer'
 import Resource from './components/Resource'
 import ResourceList from './components/ResourceList'
+import ResourcePager from './components/ResourcePager'
 import ResourceDataView from './components/ResourceDataView'
 
 export {configureStore}
@@ -17,6 +18,7 @@ export {SmartQuery}
 export {SmartDevQuery}
 export {Resource}
 export {ResourceList}
+export {ResourcePager}
 export {DataViewer}
 export {ResourceDataView}
 
@@ -30,5 +32,6 @@ export default {
     DataViewer: DataViewer,
     Resource: Resource,
     ResourceList: ResourceList,
+    ResourcePager: ResourcePager,
     ResourceDataView: ResourceDataView
 }
